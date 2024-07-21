2f522dc4564c27876ada56797e10f1f08a46ee62
Murkrow <marcocoppola02.mc@gmail.com>
2024-07-21 18:18:33 +0200

    Finished

--- widget.py
+++ widget.py
@@ -1,19 +1,48 @@
-from mosaico import widget, config
+from mosaico import widget, colors
+import datetime
 
-# Create title
-text = widget.createText()
-text.setText(config["name"])
-text.setHexColor(config["color"])
-text.translate(2,2)
-text.setFontHeight(10)
+fontName = "10x20"
+timeHeight = 20
 
-# Create items
-items = []
-for i in range(0, len(config["items"])):
-    items.append(widget.createText())
-    items[i].setFontHeight(6)
-    items[i].setText(config["items"][i])
-    items[i].translate(2, 6+ 7 * (i + 1))
+# Create text for hours
+hourText = widget.createText()
+hourText.setFont(fontName)
+hourText.moveTo(8, timeHeight)  
+
+# Create text for colon
+colonText = widget.createText()
+colonText.setFont(fontName)
+colonText.setColor(colors.red)
+colonText.moveTo(26, timeHeight) 
+
+# Create text for minutes
+minuteText = widget.createText()
+minuteText.setFont(fontName)
+minuteText.moveTo(35, timeHeight)  
+
+upRect = widget.createRectangle()
+upRect.setSize(64,2)
+upRect.translateYBy(timeHeight-2)
+
+downRect = widget.createRectangle()
+downRect.setSize(64,2)
+downRect.translateYBy(timeHeight+19)
+
+# Variable to keep track of the colon visibility state
+colon_visible = True
 
 def loop():
-    pass
+  global colon_visible
+  
+  current_time = datetime.datetime.now()
+  hourText.setText(current_time.strftime("%H"))
+  minuteText.setText(current_time.strftime("%M"))
+  
+  # Toggle the colon visibility
+  if colon_visible:
+    colonText.setText(":")
+  else:
+    colonText.setText("")
+  
+  # Update colon visibility state
+  colon_visible = not colon_visible
